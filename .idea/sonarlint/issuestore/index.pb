dfcf10095b2de359488066bf81101f3013f03f1c
Domen Kovacic <domen.kovacic01@gmail.com>
2024-01-12 14:49:00 +0100

    Added a function that fetches the robots.txt file

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -4,21 +4,14 @@
 :
 
 LICENSE.md,4/a/4a8486805915245bbbe2e3c8c1a9d680c6843a82
-;
-src/main.py,d/1/d1a7263fc38a6779482c06cdc0d4654c6182bffc
 :
 
 .gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
-G
-src/frontend/index.html,8/b/8b215f6b423b43c02973e5451ce4f8f0fe85da67
-:
-
-src/app.py,a/c/ac95095330a4a20b1f198c92db70024a198bb660
-C
-src/frontend/app.py,5/9/599459e01fbdbd9ce92b26d29b785a3bfb24bf9b
 6
 app.py,1/3/13cce7fd076299c81b4986166f3d822791c9490e
 D
 templates/index.html,f/6/f69b8ce2e31684ae06f1b3e84821e07e3dfddb2f
 @
 static/style.css,e/a/ea7b422455b8b60eb5b1123f19040f20653b36a4
+F
+templates/results.html,f/6/f603b09840103e1dd049bd43eabfee7fab087bde
--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import requests
+from urllib.parse import urlparse,urljoin
 
 app = Flask(__name__)
 @app.route('/')
-
 def index():
     return render_template('index.html')
+@app.route('/check-url', methods=['POST'])
+def check_url():
+    url = request.form['url']
+    return f"Checking URL: {url} "
 
+def fetch_robots_txt(url):
+    parsed_url = urlparse(url)
+    robots_txt = urljoin(parsed_url, '/robots.txt')
+    try:
+        request_call = requests.get(robots_txt)
+        if request_call.status_code == 200:
+            return request_call.text
+        else:
+            return f"Unable to fetch robots from {url}"
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == '__main__':
     app.run(debug=True)
