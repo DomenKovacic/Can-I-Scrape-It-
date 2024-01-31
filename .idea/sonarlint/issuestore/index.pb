2836b8b729c6c076c1570c6e10c6f1de032d3501
Domen Kovacic <domen.kovacic01@gmail.com>
2024-01-31 13:14:04 +0100

    Browser now prints robots content. Need to make it more readable.

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,11 +7,14 @@ LICENSE.md,4/a/4a8486805915245bbbe2e3c8c1a9d680c6843a82
 :
 
 .gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
-6
-app.py,1/3/13cce7fd076299c81b4986166f3d822791c9490e
 D
 templates/index.html,f/6/f69b8ce2e31684ae06f1b3e84821e07e3dfddb2f
 @
 static/style.css,e/a/ea7b422455b8b60eb5b1123f19040f20653b36a4
-F
-templates/results.html,f/6/f603b09840103e1dd049bd43eabfee7fab087bde
+C
+src/frontend/app.py,5/9/599459e01fbdbd9ce92b26d29b785a3bfb24bf9b
+:
+
+src/app.py,a/c/ac95095330a4a20b1f198c92db70024a198bb660
+6
+app.py,1/3/13cce7fd076299c81b4986166f3d822791c9490e
--- app.py
+++ app.py
@@ -1,27 +1,32 @@
 from flask import Flask, render_template, request
 import requests
-from urllib.parse import urlparse,urljoin
+from urllib.parse import urlparse, urljoin
 
 app = Flask(__name__)
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 @app.route('/check-url', methods=['POST'])
 def check_url():
     url = request.form['url']
-    return f"Checking URL: {url} "
+    robots_txt_content = fetch_robots_txt(url)
+    return f"Checking URL: {url}\nRobots.txt content:\n{robots_txt_content}"
 
 def fetch_robots_txt(url):
     parsed_url = urlparse(url)
-    robots_txt = urljoin(parsed_url, '/robots.txt')
+    if not parsed_url.scheme:
+        url = 'http://' + url
+    robots_txt_url = urljoin(url, '/robots.txt')
     try:
-        request_call = requests.get(robots_txt)
+        request_call = requests.get(robots_txt_url)
         if request_call.status_code == 200:
             return request_call.text
         else:
             return f"Unable to fetch robots from {url}"
     except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+        return f"An error occurred: {e}"
 
 if __name__ == '__main__':
     app.run(debug=True)
